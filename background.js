cce0ec8d9370bfcf194ef0ce4cb72c5898f91a7c
Ulrich Müller <uvm@talionis.net>
2022-01-05 21:11:53 +0100

    Talk to tasks

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Hi there');
+const API_KEY = 'AIzaSyC84UR2ALrHmb580Zj-sc1i7pnzGsNKBug';
+
+chrome.runtime.onInstalled.addListener((request, sender, sendResponse) => {
+    console.log('Hi there');
+    chrome.identity.getAuthToken({interactive: true}, (authToken) => {
+        let fetchUrl = `https://tasks.googleapis.com/tasks/v1/users/@me/lists?key=${API_KEY}`;
+        let fetchOptions = {
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        };
+        fetch(fetchUrl, fetchOptions)
+            .then(res => res.json())
+            .then(res => console.log(res));
+    });
 });
+
