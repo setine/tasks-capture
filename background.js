cd2a6aa7cc79414570c9e19f24fcb4cd97c5789d
Ulrich Müller <uvm@talionis.net>
2022-01-06 11:35:48 +0100

    Create a task

--- background.js
+++ background.js
@@ -1,25 +1,31 @@
 const API_KEY = 'AIzaSyC84UR2ALrHmb580Zj-sc1i7pnzGsNKBug';
 const FETCH_TASK_LISTS_URL = `https://tasks.googleapis.com/tasks/v1/users/@me/lists?key=${API_KEY}`;
+const CREATE_TASK_URL = (taskListId) => `https://tasks.googleapis.com/tasks/v1/lists/${taskListId}/tasks?key=${API_KEY}`;
 
 chrome.runtime.onInstalled.addListener((request, sender, sendResponse) => {
 });
 
 async function createTask(tabTitle, tabUrl, textSelection) {
     chrome.identity.getAuthToken({interactive: true}, (authToken) => {
-        let fetchOptions = {
-            headers: {
-                'Authorization': `Bearer ${authToken}`
-            }
+        const fetchHeaders = {
+                'Authorization': `Bearer ${authToken}`,
+                'Content-Type': 'application/json'
         };
-        const defaultTaskList = fetch(FETCH_TASK_LISTS_URL, fetchOptions)
+        const defaultTaskListPromise = fetch(FETCH_TASK_LISTS_URL, {'headers': fetchHeaders})
                 .then(res => res.json())
-                .then(res => res.items[0])
-                .then(defaultTaskList => {
-                    // TODO: Create a task
-                    console.log(defaultTaskList.id);
-                });
+                .then(res => res.items[0]);
+        defaultTaskListPromise.then(taskList => {
+            fetch(CREATE_TASK_URL(taskList.id), {
+                'headers': fetchHeaders,
+                'method': 'POST',
+                'body': JSON.stringify({
+                    'title': tabTitle,
+                    'notes': [textSelection, tabUrl].filter(Boolean).join("\n\n")
+                })
+            }).then(res => console.log(res))
+        });
+
     });
-    console.log([tabTitle, tabUrl, textSelection]);
 }
 
 async function getCurrentTab() {
