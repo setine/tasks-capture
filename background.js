56322a3bbcf704e2c9c8b432252641c8a49a56dd
Ulrich Müller <uvm@talionis.net>
2022-01-07 18:31:17 +0100

    Clang format

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 const API_KEY = 'AIzaSyC84UR2ALrHmb580Zj-sc1i7pnzGsNKBug';
-const FETCH_TASK_LISTS_URL = `https://tasks.googleapis.com/tasks/v1/users/@me/lists?key=${API_KEY}`;
-const CREATE_TASK_URL = (taskListId) => `https://tasks.googleapis.com/tasks/v1/lists/${taskListId}/tasks?key=${API_KEY}`;
+const FETCH_TASK_LISTS_URL =
+    `https://tasks.googleapis.com/tasks/v1/users/@me/lists?key=${API_KEY}`;
+const CREATE_TASK_URL = (taskListId) =>
+    `https://tasks.googleapis.com/tasks/v1/lists/${taskListId}/tasks?key=${
+        API_KEY}`;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request.message === 'capture') {
+    if (request.message === 'capture') {
         createTask(request.title, request.description);
         sendResponse();
     }
@@ -12,22 +15,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 async function createTask(title, description) {
     chrome.identity.getAuthToken({interactive: true}, (authToken) => {
         const fetchHeaders = {
-                'Authorization': `Bearer ${authToken}`,
-                'Content-Type': 'application/json'
+            'Authorization': `Bearer ${authToken}`,
+            'Content-Type': 'application/json',
         };
-        const defaultTaskListPromise = fetch(FETCH_TASK_LISTS_URL, {'headers': fetchHeaders})
-                .then(res => res.json())
-                .then(res => res.items[0]);
-        defaultTaskListPromise.then(taskList => {
+        const defaultTaskListPromise =
+            fetch(FETCH_TASK_LISTS_URL, {'headers': fetchHeaders})
+                .then((res) => res.json())
+                .then((res) => res.items[0]);
+        defaultTaskListPromise.then((taskList) => {
             fetch(CREATE_TASK_URL(taskList.id), {
                 'headers': fetchHeaders,
                 'method': 'POST',
                 'body': JSON.stringify({
                     'title': title,
-                    'notes': description
-                })
-            }).then(res => console.log(res))
+                    'notes': description,
+                }),
+            }).then((res) => console.log(res));
         });
-
     });
 }
--- popup.js
+++ popup.js
@@ -1,35 +1,34 @@
-let createTaskButton = document.getElementById("createTask");
-let titleInput = document.getElementById("title");
-let descriptionInput = document.getElementById("description");
+const createTaskButton = document.getElementById('createTask');
+const titleInput = document.getElementById('title');
+const descriptionInput = document.getElementById('description');
 
-createTaskButton.addEventListener("click", async () => {
-  chrome.runtime.sendMessage({
-      'message': 'capture',
-      'title': titleInput.value,
-      'description': descriptionInput.value,
-  });
-  window.close();
+createTaskButton.addEventListener('click', async () => {
+    chrome.runtime.sendMessage({
+        'message': 'capture',
+        'title': titleInput.value,
+        'description': descriptionInput.value,
+    });
+    window.close();
 });
 
 window.addEventListener('DOMContentLoaded', async (event) => {
-  const tab = await getCurrentTab();
-  const selection = await getCurrentSelection(tab.id);
-  titleInput.value = tab.title;
-  descriptionInput.value = [selection, tab.url]
-        .filter(Boolean).join("\n\n");
-  titleInput.select();
+    const tab = await getCurrentTab();
+    const selection = await getCurrentSelection(tab.id);
+    titleInput.value = tab.title;
+    descriptionInput.value = [selection, tab.url].filter(Boolean).join('\n\n');
+    titleInput.select();
 });
 
 async function getCurrentTab() {
-  const queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+    const queryOptions = {active: true, currentWindow: true};
+    const [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
 }
 
 async function getCurrentSelection(tabId) {
     const [selection] = await chrome.scripting.executeScript({
         'func': () => window.getSelection().toString(),
-        'target': {'tabId': tabId}
+        'target': {'tabId': tabId},
     });
     return selection.result;
 }
